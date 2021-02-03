cb246496c7995690f660bac894a9b2051e0152d1
PabloSSena <pablosilvasena@gmail.com>
2021-02-03 16:44:09 -0300

    Feito link pra home e animaçao na intro

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
 import { createAnimation, Animation } from '@ionic/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
 })
-export class MenuPage implements OnInit {
+export class MenuPage implements AfterViewInit {
+  @ViewChild('element', {static:false}) element:ElementRef;
+  animation: Animation;
+  isPlaying = false;
 
-  constructor() { }
+  constructor(private animationCtrl: AnimationController) { }
   
-  ngOnInit() {
-    const animation: Animation = createAnimation('')
-    .addElement(document.querySelector('.element'))
-    .duration(Infinity)
-    .fromTo('transform','translateY(0px)','translateY(100px)');
+  ngAfterViewInit() {
+    this.animation = this.animationCtrl.create('myanimation')
+    this.animation.addElement(this.element.nativeElement)
+    .duration(800)
+    .easing('ease-out')
+    .iterations(Infinity)
+    .fromTo('transform', 'translateY(0px)', 'translateY(-30px)')
   }
 
+  toogleAnimation() {
+    if (this.isPlaying) this.animation.pause()
+
+    else this.animation.play();
+    
+    this.isPlaying = !this.isPlaying;
+  }
+
+  next() {
+    
+  }
 }
