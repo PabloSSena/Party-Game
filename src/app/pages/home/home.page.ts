ce9b0fd1047a509d45c3b9f9dbe725251ed86ec3
PabloSSena <pablosilvasena@gmail.com>
2021-02-03 18:46:59 -0300

    Feito a home e colocado dois cards

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class HomePage{
+  features = [
+    {
+      title: "VERDADE OU DESAFIO",
+      icon: "https://i.pinimg.com/474x/18/9d/c1/189dc13caee842c1580fd2133b48d75f.jpg"
+    },
+    {
+      title: "VERDADE OU DESAFIO PICANTE",
+      icon: "https://st.depositphotos.com/1000345/2217/v/600/depositphotos_22170357-stock-illustration-pepper-in-open-mouth.jpg"
+    } 
+  ]
 }
